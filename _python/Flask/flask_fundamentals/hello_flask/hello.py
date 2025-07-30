8e44717eed4cdd567d88348dce9a9a05ff2e5606
Omymakurd <omyma_1994@hotmail.com>
2025-07-30 17:02:27 +0300

    Updated hello.py with fixed routes and repeat endpoint

---
+++ _python/Flask/flask_fundamentals/hello_flask/hello.py
@@ -0,0 +1,18 @@
+from flask import Flask 
+app=Flask(__name__)
+
+@app.route('/')
+
+def hello():
+    return 'Hello World!'
+@app.route('/Champion')
+def champion():
+    return "Champion!"
+@app.route('/say/<name>')
+def say_hi(name):
+    return f"Hi {name}"
+@app.route('/repeat/<int:num>/<word>')
+def repeat_word(num,word):
+    return (word + ' ') * num
+if __name__ == '__main__':
+    app.run(debug=True)
