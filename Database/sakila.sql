9b9e440c56f1c6ae10cc39ff2a100f0aab904801
Omymakurd <omyma_1994@hotmail.com>
2025-08-28 10:35:36 +0300

    Add Sakila database assignment

---
+++ Database/sakila.sql
@@ -0,0 +1,78 @@
+select customer.first_name,customer.last_name,customer.email,address.address,address.city_id
+from customer
+join address
+on customer.address_id=address.address_id
+where address.city_id=312
+-------------------------------------
+select film.title,film.description,film.release_year,film.rating,film.special_features,category.name
+from film 
+join film_category 
+on film.film_id=film_category.film_id
+join category
+on category.category_id=film_category.category_id
+where category.name="Comedy"
+--------------------------------------------------------
+select actor.actor_id,CONCAT(actor.first_name,' ',actor.last_name) as name,film.title,film.description,film.release_year
+from film 
+join film_actor
+on film.film_id=film_actor.film_id
+join actor
+on actor.actor_id=film_actor.actor_id
+where actor.actor_id=5
+---------------------------------------------------------
+select customer.first_name,customer.last_name,customer.email,address.address
+from customer
+join address
+on customer.address_id=address.address_id
+where customer.store_id =1
+and address.city_id in(1,42,312,459)
+-----------------------------------------------------------
+select film.title,film.description,film.release_year,film.rating,film.special_features
+from film
+join film_actor
+on film.film_id=film_actor.film_id
+where film.rating='G'
+and film.special_features  like '%Behind the Scenes'
+and film_actor.actor_id=15
+-----------------------------------------------------------
+select film.film_id,film.title,actor.actor_id,concat( actor.first_name,' ',actor.last_name) as name
+from film 
+join film_actor
+on film.film_id=film_actor.film_id
+join actor
+on actor.actor_id=film_actor.actor_id
+where film_actor.film_id=369
+----------------------------------------------------------------
+select film.title,film.description,film.release_year,film.rating,film.special_features,category.name
+from film
+join film_category
+on film.film_id =film_category.film_id
+join category
+on category.category_id=film_category.category_id
+where category.name ='Drama'
+-------------------------------------------------------
+select film.title,film.description,film.release_year,film.rating,film.special_features,category.name,concat( actor.first_name,' ',actor.last_name) as name
+from film
+join film_category
+on film.film_id =film_category.film_id
+join category
+on category.category_id=film_category.category_id
+join film_actor
+on film.film_id=film_actor.film_id
+join actor
+on actor.actor_id=film_actor.actor_id
+where category.name ='Action'
+and actor.first_name='SANDRA'
+and actor.last_name='KILMER'
+------------------------------------------------------
+select * from film_actor
+select * from actor
+select * from address
+select * from customer
+select * from city
+select * from country
+select * from film
+select * from category
+select * from film_category
+select * from film actor
+select * from film_text
