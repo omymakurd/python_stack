7890310e2ed43863fa2796c6e4e3f1cc6372e458
Omymakurd <omyma_1994@hotmail.com>
2025-09-04 15:39:13 +0300

    Add users with Templates Assignment

--- assignments_and_projects/single_model_orm/users_app/urls.py
+++ assignments_and_projects/single_model_orm/users_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("add_user",views.add_user,name="add_user")
 ]
--- assignments_and_projects/single_model_orm/users_app/views.py
+++ assignments_and_projects/single_model_orm/users_app/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse,redirect
+from .models import Users
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello omayma')
+    context={
+        "all_users":Users.objects.all()
+    }
+    return render(request,'index.html',context)
+def add_user(request):
+    if request.method== 'POST' :
+        first_user_name=request.POST['first_name']
+        last_user_name=request.POST['last_name']
+        email=request.POST['email_address']
+        user_age=request.POST['age']
+        Users.objects.create(first_name=first_user_name,
+                             last_name=last_user_name,
+                             email_address=email,
+                             age=user_age)
+        return redirect('/')
+    return redirect('/')
