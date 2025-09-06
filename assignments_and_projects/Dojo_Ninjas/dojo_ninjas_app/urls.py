00a7262549c665efe00fb61f91ad754b2894a1ec
Omymakurd <omyma_1994@hotmail.com>
2025-09-06 16:50:28 +0300

    Add Dojos & Ninjas with Template and update previous assignment

--- assignments_and_projects/Dojo_Ninjas/dojo_ninjas_app/urls.py
+++ assignments_and_projects/Dojo_Ninjas/dojo_ninjas_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('',views.index)
+    path('',views.index),
+    path('add_dojo',views.add_dojo),
+    path('add_ninja',views.add_ninja),
+    path('delete_dojo/<int:dojo_id>/',views.delete_dojo)
 ]
--- assignments_and_projects/Dojo_Ninjas/dojo_ninjas_app/views.py
+++ assignments_and_projects/Dojo_Ninjas/dojo_ninjas_app/views.py
@@ -1,4 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Dojos,Ninjas
+from django.db.models import Count
 def index(request):
-    return render(request,"index.html")
+    context={
+        "dojos":Dojos.objects.annotate(ninjas_count=Count('ninjas'))
+    }
+    return render(request,"index.html",context)
+def add_dojo(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        city=request.POST['city']
+        stat=request.POST['stat']
+        desc=request.POST['desc']
+        Dojos.objects.create(
+            name=name,
+            city=city,
+            stat=stat,
+            desc=desc
+        )
+        return redirect('/')
+    return redirect('/')
+
+def add_ninja(request):
+    if request.method=='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        dojo_id=request.POST['dojo']
+        dojo=Dojos.objects.get(id=dojo_id)
+        Ninjas.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            dojo=dojo
+        )
+        return redirect('/')
+    return redirect('/')
+def delete_dojo(request,dojo_id):
+    dojo=Dojos.objects.get(id=dojo_id)
+    dojo.delete()
+    return redirect('/')
+
 # Create your views here.
